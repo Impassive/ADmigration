23f4d37598acdc2bcff07994b9fb0b80f6bee50b
mikhail <tmv1995@gmail.com>
2018-07-27 11:29:26 +0300

    complete migration prog

--- migrate.py
+++ migrate.py
@@ -1,26 +1,31 @@
 import csv
 
 f = open("test.ldif", "w+")
-with open('test.csv') as csvfile:
+with open('people.csv') as csvfile:
      reader = csv.DictReader(csvfile)
      baseuid = 10008
      base = str(baseuid)
      for row in reader:
-         string = (row['username'])
-         user = string.split('.')
+         usrstr = (row['username'])
+         user = usrstr.split('.')
          givenname = user[0]
          sn = user[1]
-         f.write("dn: cn="+givenname+" " + sn+",ou=Employees,dc=wdt,dc=com\n"
-                 "cn: "+givenname+" "+sn+"\n"
+         title = (row['title'])
+         unit = (row['unit'])
+         f.write("dn: cn="+givenname.capitalize()+" " + sn.capitalize()+",ou=Employees,dc=wdt,dc=com\n"
+                 "cn: "+givenname.capitalize()+" "+sn.capitalize()+"\n"
                  "gidnumber: 10000\n"
-                 "givenname: "+givenname+"\n"
+                 "givenname: "+givenname.capitalize()+"\n"
                  "homedirectory: /home/"+givenname+"."+sn+"\n"
                  "loginshell: /bin/bash\n"
+                 "mail: "+givenname+"."+sn+"@weigandt-consulting.com\n"
                  "objectclass: posixAccount\n"
                  "objectclass: inetOrgPerson\n"
                  "objectclass: organizationalPerson\n"
                  "objectclass: person\n"
-                 "sn: "+sn+"\n"
+                 "physicaldeliveryofficename: "+unit+"\n"
+                 "sn: "+sn.capitalize()+"\n"
+                 "title: "+title+"\n" 
                  "uid: "+givenname+"."+sn+"\n"
                  "uidnumber: "+base+"\n\n")
          baseuid+=1
