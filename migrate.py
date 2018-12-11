a75201b564c32e1a1d45561e9cc7ad7e97f0d280
mikhail <tmv1995@gmail.com>
2018-12-11 10:43:23 +0300

    add mailer

--- migrate.py
+++ migrate.py
@@ -2,7 +2,6 @@ import csv
 import random
 import string
 from passlib.hash import ldap_salted_sha1 as ssha
-import smtplib, ssl
 
 def password_generation(length):
     count = 0
@@ -18,44 +17,27 @@ def password_generation(length):
         pwd += random.choice(everything)
         count += 1
         continue
-    print(pwd)
     return pwd
 
 
 # Hash Password for RFC 2307 format ssha
 def hashed_password(password):
     hash = ssha.hash(password)
-    print(hash)
-    global u_hash
-    u_hash = hash
     return hash
 
 
-def mail_notification(mailBody):
-    port = 465  # For SSL
-    login = "mikhail.toporov@weigandt-consulting.com"
-    password = 'F7hI2ZSGcpma'
-    smtp_server = "smtp.googlemail.com"
 
-    # Create a secure SSL context
-    context = ssl.create_default_context()
-    print("Login as: " + login + "\n" + "Pass: " + password)
-    with smtplib.SMTP(smtp_server, port) as server:
-        server.ehlo()  # Can be omitted
-        server.starttls(context=context)
-        server.ehlo()  # Can be omitted
-        server.login(login, password)
-        server.sendmail(login, login, mailBody)
-        server.quit()
-        print('mail has been sent')
 
 # Parse input csv and format to ldif
-f = open("test.ldif", "w+")
-with open('test.csv') as csvfile:
+f = open("./data/test.ldif", "w+")
+# Open another file to save email notification info
+mail = open('./data/mailer.csv', mode='w')
+with open('./data/test.csv') as csvfile:
+     writer = csv.writer(mail, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
+     writer.writerow(["username", "clearPassword", "ssha hash", "email"])
      reader = csv.DictReader(csvfile)
      baseuid = 30001
      base = str(baseuid)
-     mailBody = 'Next users were created: ' + "\n"
      for row in reader:
          usrstr = (row['username'])
          user = usrstr.split('.')
@@ -83,11 +65,9 @@ with open('test.csv') as csvfile:
                  "uid: " + givenname + "." + sn + "\n"
                  "uidnumber: " + base + "\n\n")
          baseuid+=1
-         mailBody+="\n" + "Username: " + givenname + "." + sn
-         mailBody+="\n" + "Password: " + str(clearPassword)
-         mailBody+="\n" + "Hash: " + str(sshaPassword)
          base = str(baseuid)
-     print(mailBody)
-     mail_notification(mailBody)
+         writer.writerow([givenname.lower() + "." + sn.lower(), str(clearPassword), str(sshaPassword), ""])
+
+mail.close()
 f.close()
 
---
+++ notification.py
@@ -0,0 +1,31 @@
+import csv
+import smtplib, ssl
+
+def mail_notification(mailBody, to):
+    port = 465  # For SSL
+    login = "****"
+    password = '****'
+    smtp_server = "smtp.googlemail.com"
+    # Create a secure SSL context
+    context = ssl.create_default_context()
+    print("Login as: " + login + "\n" + "Pass: " + password)
+    with smtplib.SMTP_SSL("smtp.gmail.com", 465, context=context) as server:
+        server.login(login, password)
+        server.sendmail(from_addr=login, to_addrs=to, msg=mailBody)
+        print('mail has been sent')
+
+
+with open('./data/mailer.csv') as csvfile:
+    reader = csv.DictReader(csvfile)
+    for row in reader:
+        user = (row['username'])
+        clearPassword = (row['clearPassword'])
+        hash = (row['ssha hash'])
+        email = (row['email'])
+        mailBody = "Subject: LDAP script: test email notification\n\nBelow is your new user account: " + "\n"
+
+        mailBody += "\n" + "Username: " + user
+        mailBody += "\n" + "Password: " + clearPassword
+        mailBody += "\n" + "Hash: " + hash
+        print(mailBody)
+        mail_notification(mailBody,"tmv1995@gmail.com")
